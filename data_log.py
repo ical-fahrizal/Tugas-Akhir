ed1514d9263f160526c5a1afeef5849a3f6f9dee
WisnuMaul0112 <mwisnu543@gmail.com>
2022-06-30 18:28:55 +0700

    datalog

--- data_log.py
+++ data_log.py
@@ -1,16 +1,19 @@
 import os
 import time 
-from time import sleep
 from datetime import datetime
 
 filename = datetime.now().strftime('data logger-%Y-%m-%d.csv')
 file = open("data_log/{}".format(filename), "a")
 i=0
 if os.stat("data_log/{}".format(filename)).st_size == 0:
-        file.write("Time,Sensor1,Sensor2,Sensor3,Sensor4,Sensor5\n")
+        file.write("Waktu,Pengunjung,Pelanggar Masker,Keterangan\n")
 while True:
     i=i+1
     now = datetime.now()
-    file.write(str(now)+","+str(i)+","+str(-i)+","+str(i-10)+","+str(i+5)+","+str(i*i)+"\n")
-    file.flush()
-    time.sleep(5)
+    if (datetime.now().second % 5 == 0):
+        file.write(str(now.strftime("%H:%M:%S"))+","+str(i)+","+str(-i)+","+str(i-10)+"\n")
+        file.flush()
+        time.sleep(1)
+
+    print(now)
+    time.sleep(.2)
---
+++ time.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+while True:
+    waktu = datetime.now().strftime('%S')
+    print(waktu)
+    if (datetime.now().minute % 1 == 0):
+        print("10 detik berlalu")
+    sleep(1)
+    
